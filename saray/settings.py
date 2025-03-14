e85d6a4d6b32ce6d0f4e893b64532f606d37fa70
unknown <you@example.com>
2025-03-13 22:08:18 -0500

    doss

--- saray/settings.py
+++ saray/settings.py
@@ -117,8 +117,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -128,8 +126,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'ingles/static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-ALLOWED_HOSTS = ['localhost','web-production-e2a45.up.railway.app']
+ALLOWED_HOSTS = ['localhost','inglesss-production.up.railway.app']
